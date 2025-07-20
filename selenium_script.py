cf32eaf4ba62895574dd1f50645433aa09ba46fd
Akinlua <akinluaolorunfunminiyi@gmail.com>
2025-07-20 19:40:01 +0100

    first commit

--- selenium_script.py
+++ selenium_script.py
@@ -31,6 +31,10 @@ class WebsiteOpener:
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         
         # Use direct path to Chrome on Mac
         if os.path.exists("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"):
